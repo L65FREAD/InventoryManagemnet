1c6e713e93baaa078b987bcc82e02a83ec8b1b45
ayus <l65.fread@gmail.com>
2020-12-01 10:43:01 +0545

    Committed to the branch

--- src/com/skillup/BodyShell.java
+++ src/com/skillup/BodyShell.java
@@ -1,5 +1,42 @@
 package com.skillup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class BodyShell {
 
+    private static HashMap<String,Integer> bodyShellCount = new HashMap<>();
+
+    private int totalBodyShellCount;
+
+    private static final ArrayList<String> availableShells = new ArrayList<>
+            (Arrays.asList("SPORTS","SUV","CLASSIC","ATV","DUNE BUGGY","CRAWLERS","MILITARY","TRUCKS"));
+
+    private final int price;
+    private String bodyShellType;
+
+    public BodyShell(String bodyShellType) {
+        this.bodyShellType = bodyShellType;
+        validateBodyShell(bodyShellType);
+        initializeHashMap();
+
+        price =0;
+    }
+
+     private void initializeHashMap() {
+         for (String i : availableShells) {
+             bodyShellCount.put(i, 0);
+         }
+    }
+
+    public static void addBodyShell(String bodyShellType){
+
+    }
+    public boolean validateBodyShell(String bodyShellType){
+        if (availableShells.contains(bodyShellType.toUpperCase())){
+            return true;
+        }
+        return false;
+    }
 }
--- src/com/skillup/Cache.java
+++ src/com/skillup/Cache.java
@@ -1,14 +1,21 @@
 package com.skillup;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Cache {
     public static void main(String[] args) {
-        Wheels.addWheels(2,0);
-        Wheels wheels = new Wheels(true);
-        System.out.println(wheels.getPrice());
-        int x = 0;
-        if ((--x)<0) System.out.println(x);
+//        Wheels.addWheels(2,0);
+//        Wheels wheels = new Wheels(true);
+//        System.out.println(wheels.getPrice());
+//        int x = 0;
+//        if ((--x)<0) System.out.println(x);
+
+
+         final ArrayList<String> availableShells = new ArrayList<>
+                (Arrays.asList("SPORTS","SUV","CLASSIC","ATV","DUNE BUGGY","CRAWLERS","MILITARY","TRUCKS"));
+        String i= "classic";
+        if (availableShells.contains(i.toUpperCase())) System.out.print("Hello there mate");
     }
 }
